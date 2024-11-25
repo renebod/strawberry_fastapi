d2c0e588df615fc2daecf3666c035161369cccbf
René Bod <rene.bod@mac.com>
2024-11-25 14:14:01 +0100

    Not better version

--- strawberry_fastapi/utils/dbfill.py
+++ strawberry_fastapi/utils/dbfill.py
@@ -1,8 +1,26 @@
-import db
+# import db
+import couchdb
+import os
 from uuid import uuid4
 
 
+username = os.environ.get('COUCHDB_USERNAME', 'admin')
+password = os.environ.get('COUCHDB_PASSWORD', 'password')
+dbhost =  os.environ.get('COUCHDB_HOSTNAME', 'couchdb')
+port = os.environ.get('COUCHDB_PORT', '5984')
+couchserver = couchdb.Server(f"http://{username}:{password}@{dbhost}:{port}/")
+
+
+def get_database(db_name):
+    return couchserver[db_name]
+
 databases = ['functional_objects', 'technical_objects', 'reference_objects']
+
+for database in databases:
+        if database in couchserver:
+            db = couchserver[database]
+        else:
+            db = couchserver.create(database)
 
 ref_db = db.get_database('reference_objects')
 
